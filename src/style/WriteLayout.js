dd3a188f3cdfec93edb5fcbcd981232d618e0bd0
kimaydev <kimaydev@gmail.com>
2023-07-03 15:05:41 +0900

    refactor : MyPlantWrite 페이지 퍼블리싱 코드 정리

--- src/style/WriteLayout.js
+++ src/style/WriteLayout.js
@@ -1,6 +1,70 @@
 import styled from "@emotion/styled";
-import { subColor, height, borderColor } from "./GlobalStyle";
+import { subColor, height } from "./GlobalStyle";
 
+// MyPlant
+export const MyPlantWriteFir = styled.div`
+  margin-bottom: 30px;
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+  & > div {
+    .ant-input {
+      height: ${height.inputHeight};
+      padding: 0 15px;
+      border-radius: 10px;
+      &::placeholder {
+        color: ${subColor.colorGrayBold};
+      }
+    }
+  }
+  .ant-picker {
+    height: ${height.inputHeight};
+    padding: 0 15px;
+    border-radius: 10px;
+  }
+  .ant-upload-wrapper {
+    .ant-upload-list {
+      .ant-upload {
+        background: ${subColor.colorWhite};
+        border-radius: 10px;
+        margin-bottom: 0;
+        &:last-of-type {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+  textarea.ant-input {
+    resize: none;
+    min-height: 20vh;
+    height: 100%;
+    padding: 12px 15px;
+    line-height: 1.5;
+    border-radius: 10px;
+    &::placeholder {
+      color: ${subColor.colorGrayBold};
+    }
+  }
+`;
+export const MyPlantWriteTxt = styled.div`
+  margin-bottom: 10px;
+  font-weight: 700;
+  font-size: 1.6rem;
+  & > p {
+    font-size: 0.75em;
+    font-weight: 400;
+    color: ${subColor.colorRed};
+    margin-top: -3px;
+  }
+`;
+
+export const MyPlantWtP = styled.p`
+  font-size: 12px;
+  font-weight: 400;
+  margin-bottom: 15px;
+  color: ${subColor.colorRed};
+`;
+// Todo
 export const TodoWriteFir = styled.div`
   margin-bottom: 30px;
   &:last-of-type {
@@ -63,11 +127,4 @@ export const TodoWriteTxt = styled.div`
   margin-bottom: 10px;
   font-weight: 700;
   font-size: 1.6rem;
-`;
-
-export const MyPlantWtP = styled.p`
-  font-size: 12px;
-  font-weight: 400;
-  margin-bottom: 15px;
-  color: ${subColor.colorRed};
 `;
