bce4e24f5ffb7a7fb98f12b8820398cc57af0476
kimaydev <kimaydev@gmail.com>
2023-07-03 12:28:19 +0900

    refactor : TodoWrite 페이지 퍼블리싱 코드 정리

--- src/style/WriteLayout.js
+++ src/style/WriteLayout.js
@@ -1,14 +1,68 @@
 import styled from "@emotion/styled";
-import { subColor } from "./GlobalStyle";
+import { subColor, height, borderColor } from "./GlobalStyle";
 
 export const TodoWriteFir = styled.div`
-  margin: 15 0 0 22px;
+  margin-bottom: 30px;
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+  .ant-select {
+    width: 100%;
+    height: ${height.inputHeight};
+    .ant-select-selector {
+      align-items: center;
+      height: 100%;
+      padding: 0 15px;
+      border-radius: 10px;
+      .ant-select-selection-search-input {
+        height: 100% !important;
+      }
+      .ant-select-selection-placeholder {
+        margin: 0;
+        color: ${subColor.colorGrayBold};
+      }
+    }
+  }
+  .plant-dete {
+    display: flex;
+    align-items: center;
+    & > li {
+      margin-right: 5px;
+      &:last-of-type {
+        margin-right: 0;
+      }
+      .ant-picker {
+        height: ${height.inputHeight};
+        padding: 0 15px;
+        border-radius: 10px;
+      }
+    }
+  }
+  textarea.ant-input {
+    resize: none;
+    min-height: 20vh;
+    height: 100%;
+    padding: 12px 15px;
+    line-height: 1.5;
+    border-radius: 10px;
+    &::placeholder {
+      color: ${subColor.colorGrayBold};
+    }
+  }
+  .ant-checkbox-group {
+    display: block;
+    .ant-checkbox-wrapper {
+      margin-right: 5px;
+      &:last-of-type {
+        margin-right: 0;
+      }
+    }
+  }
 `;
 export const TodoWriteTxt = styled.div`
   margin-bottom: 10px;
   font-weight: 700;
   font-size: 1.6rem;
-  margin-top: 25px;
 `;
 
 export const MyPlantWtP = styled.p`
